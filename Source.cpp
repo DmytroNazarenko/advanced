654ccdba94b87a8dbed3270df84c3040bedf8980
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2017-01-09 12:07:53 +0200

    upd1

--- Source.cpp
+++ Source.cpp
@@ -22,29 +22,31 @@ void output(std::ostream &outputStream, const int &answer) {
 
 int findMaxPeopleCount(const int people, const int buses, const int busCapacity, const std::vector<int> peopleVolumes) {
 
-    std::vector<std::vector<std::vector<int> > > maxPeopleCount(buses + 5, std::vector<std::vector<int> >(people + 5, std::vector<int>(busCapacity + 5)));
+    std::vector<std::vector<std::vector<int> > > maxPeopleCount(buses + 1, std::vector<std::vector<int> >(people + 1, std::vector<int>(busCapacity + 1)));
 
     maxPeopleCount[0][0][0] = 0;
     for (int filledBusesCount = 0; filledBusesCount < buses; ++filledBusesCount) {
         for (int analyzedPeopleCount = 0; analyzedPeopleCount <= people; ++analyzedPeopleCount) {
-            if (filledBusesCount != 0)
+            if (filledBusesCount != 0) {
                 maxPeopleCount[filledBusesCount][analyzedPeopleCount][0] =
-                maxPeopleCount[filledBusesCount - 1][analyzedPeopleCount][busCapacity];
-            else
+                    maxPeopleCount[filledBusesCount - 1][analyzedPeopleCount][busCapacity];
+            } else {
                 maxPeopleCount[filledBusesCount][analyzedPeopleCount][0] = 0;
+            }
             for (int usedVolumeInBus = 1; usedVolumeInBus <= busCapacity; ++usedVolumeInBus) {
                 maxPeopleCount[filledBusesCount][analyzedPeopleCount][usedVolumeInBus] =
                     maxPeopleCount[filledBusesCount][analyzedPeopleCount][usedVolumeInBus - 1];
-                if (analyzedPeopleCount > 0)
+                if (analyzedPeopleCount > 0) {
                     maxPeopleCount[filledBusesCount][analyzedPeopleCount][usedVolumeInBus] =
-                    std::max(maxPeopleCount[filledBusesCount][analyzedPeopleCount][usedVolumeInBus],
-                        maxPeopleCount[filledBusesCount][analyzedPeopleCount - 1][usedVolumeInBus]);
-
-                if (analyzedPeopleCount > 0 && usedVolumeInBus >= peopleVolumes[analyzedPeopleCount - 1])
+                        std::max(maxPeopleCount[filledBusesCount][analyzedPeopleCount][usedVolumeInBus],
+                            maxPeopleCount[filledBusesCount][analyzedPeopleCount - 1][usedVolumeInBus]);
+                }
+                if (analyzedPeopleCount > 0 && usedVolumeInBus >= peopleVolumes[analyzedPeopleCount - 1]) {
                     maxPeopleCount[filledBusesCount][analyzedPeopleCount][usedVolumeInBus] =
-                    std::max(maxPeopleCount[filledBusesCount][analyzedPeopleCount][usedVolumeInBus],
-                        maxPeopleCount[filledBusesCount][analyzedPeopleCount - 1][usedVolumeInBus
-                        - peopleVolumes[analyzedPeopleCount - 1]] + 1);
+                        std::max(maxPeopleCount[filledBusesCount][analyzedPeopleCount][usedVolumeInBus],
+                            maxPeopleCount[filledBusesCount][analyzedPeopleCount - 1][usedVolumeInBus
+                            - peopleVolumes[analyzedPeopleCount - 1]] + 1);
+                }
             }
         }
     }
@@ -56,5 +58,6 @@ int main()
     std::vector<int> peopleVolumes;
     int peopleCount, busCount, busCapacity;
     input(std::cin, peopleCount, busCount, busCapacity, peopleVolumes);
-    output(std::cout, findMaxPeopleCount(peopleCount, busCount, busCapacity, peopleVolumes));
+    int answer = findMaxPeopleCount(peopleCount, busCount, busCapacity, peopleVolumes);
+    output(std::cout, answer);
 }
